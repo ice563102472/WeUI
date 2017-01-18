d82b88b0d995f92c58b0a66244956f506c31615e
ice563102472 <563102472@qq.com>
2017-01-18 14:20:05 +0800

    weui

---
+++ src/main/java/com/linda/rpc/webui/utils/Const.java
@@ -0,0 +1,26 @@
+package com.linda.rpc.webui.utils;
+
+/**
+ * Created by lin on 2016/12/16.
+ */
+public class Const {
+
+    public static final int HOST_STATUS_ON = 1;
+
+    public static final int HOST_STATUS_OFF = 0;
+
+    public static final int HOST_STATUS_ALL = -1;
+
+
+
+    public static final int SERVICE_OK = 1;
+
+    public static final int SERVICE_ERR = 0;
+
+    public static final int SERVICE_ALL = -1;
+
+
+    public static final int CODE_SUCCESS = 200;
+
+    public static final int CODE_PARAM_ERROR = 309;
+}
